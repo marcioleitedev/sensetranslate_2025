80753e4c79d324e2325fe409aa79e724d80dc55f
Marcio Barbosa Leite <marcio.leite@akrkpromotora.local>
2025-03-29 19:00:29 -0300

    jwt authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Models\User;
+use Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if ($token = JWTAuth::attempt($credentials)) {
+            return response()->json(compact('token'));
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function getUser()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ServicoController;
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->get('user', [AuthController::class, 'getUser']);
+Route::post('logout', [AuthController::class, 'logout']);
 
-Route::get('/servicos', [ServicoController::class, 'index']);
+Route::middleware('auth:api')->get('dashboard', function (Request $request) {
+    return response()->json(['message' => 'Welcome to the admin dashboard!']);
+});
